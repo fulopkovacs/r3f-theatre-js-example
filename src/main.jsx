0b191879b4e05dafe6a5153b14102d1ed351d8f4
Fülöp Kovács <kovacs.fulop@gmail.com>
2022-06-22 14:54:23 +0200

    Add prettier

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import studio from '@theatre/studio'
+import extension from '@theatre/r3f/extension'
+studio.extend(extension)
+studio.initialize();
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback={null}>
+      <App />
+    </Suspense>
   </React.StrictMode>
 )
